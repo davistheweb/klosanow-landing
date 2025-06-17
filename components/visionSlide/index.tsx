a8e64485d611e16288db97973bf2e93e56a67c54
davistheweb <josiahdave001@gmail.com>
2025-06-17 18:05:00 +0100

    updated about slides

--- components/visionSlide/index.tsx
+++ components/visionSlide/index.tsx
@@ -78,7 +78,7 @@ export default function VisionSlide(): React.JSX.Element {
                     fontWeight={500}
                     textAlign={{ base: "center" }}
                     color="black.60"
-                    zIndex={20}
+                    zIndex={100}
                   >
                     {title}
                   </Heading>
