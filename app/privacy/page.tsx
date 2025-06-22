f2a2dc82e2acd9c664c1493532870a83539a00b9
davistheweb <josiahdave001@gmail.com>
2025-06-22 10:38:58 +0100

    :

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -2,242 +2,33 @@
 
 import * as React from "react";
 import { Text, Box, Heading } from "@chakra-ui/react";
+import { sections } from "@/utils";
 
 const PrivacyPage: React.FC = () => (
-  <>
-    {/* <Box
-      //   bgColor={"primary.50"}
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      flexDirection="column"
-      mt={{ base: "1rem", md: "6rem" }}
-      px={{ base: "2rem", md: "4rem" }}
-    >
-      <Heading textAlign="center" fontSize={{ base: "2xl", md: "4xl" }}>
-        <Text>Empowering Learners and Tutors Through Online Education</Text>
-      </Heading>
-    </Box> */}
-    <Box
-      as="div"
-      mt={{ base: "1rem", md: "3rem" }}
-      px={{ base: "1rem", md: "6rem" }}
-      py="3rem"
-    >
-      <Heading fontWeight={700} fontSize={{ base: "3xl", md: "1xl" }}>
-        <Text>Privacy Policy</Text>
-      </Heading>
-      <Text mt={{ base: "1rem", md: "2rem" }}>
-        Below is Klosanow&apos;s Privacy Policy, which explains what personal
-        information we collect and how we use the information. To protect your
-        privacy, Klosanow follows different principles in accordance with
-        worldwide practices for user privacy and data protection.
-      </Text>
-      <Text mt={{ base: "1rem", md: "2rem" }}>
-        This Privacy Policy covers the use of the Klosanow website (the
-        &quot;Site&quot;) and the products, software, data feeds, and services
-        (collectively with the Site, the &quot;Service&quot;) provided by
-        Klosanow . (&quot;Klosanow,&quot; &quot;We,&quot; &quot;Our&quot; or
-        &quot;Us&quot;), including how Klosanow treats information, including
-        personal information, that we collect and receive
-      </Text>
-      <Text mt={{ base: "1rem", md: "2rem" }}>
-        The term &quot;Personal Information&quot;, as used in this Privacy
-        Policy, refers to any information that can be used to identify a
-        specific person, or any anonymous information (e.g. IP address) that is
-        linked to a specific person. Personal Information does not include
-        information that has been aggregated or made anonymous such that it can
-        no longer be reasonably associated with a specific person. This policy
-        in no way restricts or limits our collection and use of aggregate
-        information, and we may share aggregate data about our users with third
-        parties for various purposes.
-      </Text>
-      <Text mt={{ base: "1rem", md: "2rem" }}>
-        This Privacy Policy is part of and incorporated into Klosanow&apos;s
-        Terms of Use (&quot;Terms of Use&quot;).
-      </Text>
-      <Text mt={{ base: "1rem", md: "2rem" }}>
-        Klosanow values your trust and confidence. We are committed to
-        safeguarding the privacy of your information at all times. You may add,
-        delete or modify your Personal Information at any time. Subject to your
-        permission or as specifically stated in this Privacy Policy, we will not
-        sell, exchange or otherwise share your Personal Information with third
-        parties for any reason.
-      </Text>
-      <Heading
-        fontWeight={700}
-        fontSize={{ base: "3xl", md: "1xl" }}
-        mt={{ base: "1rem", md: "2rem" }}
-      >
-        <Text>Information Collection and How it is Used</Text>
-      </Heading>
-      <Text mt={{ base: "1rem", md: "2rem" }}>
-        <b>•</b> Personal Information such as your name, username, email
-        address,phone number, password
-      </Text>
-      <Text mt={{ base: "1rem", md: "2rem" }}>
-        <b>•</b> Information Sent to Us by Your Web Browser. We collect
-        information that is sent to us automatically by your Web browser. This
-        information typically includes your IP address, the name and version of
-        your operating system, the name and version of your browser, the date
-        and time of your visit and the pages on this Site you visit. Please
-        check your browser if you want to learn what information your browser
-        sends or how to change your settings. This information does not identify
-        you personally. Generally, we use this information to improve this Site
-        and make it more compatible with the technology used by our visitors.
-        However, we may link information sent by your browser to information
-        that identifies you personally. For example, if you are a registered
-        member, we may link your IP address to information that identifies you
-        personally. In addition, if we suspect criminal activity, we may share
-        our server logs with the appropriate investigative authorities who could
-        use that information to trace and identify you.
-      </Text>
-      <Text mt={{ base: "1rem", md: "2rem" }}>
-        <b>•</b> Cookies and Other Similar Technologies: We (or our third-party
-        service providers) may usevarious technologies to collect and store
-        information when you visit our Service, including clear GIFs (also known
-        as “web beacons”), “tags”, “scripts”, and “cookies”. We also make use of
-        persistent secure cookies: a persistent cookie remains after you close
-        your browser (although they can be removed) and may be used by your
-        browser to identify you on subsequent visits to the Service. We may also
-        use, collect and store information locally onyour device using
-        mechanisms such as browser web storage (including HTML 5). Like many
-        services, Klosanow uses these technologies to tailor the Service for
-        you, and to help the Service work better for you.
-      </Text>
-      <Text mt={{ base: "1rem", md: "2rem" }}>
-        <b>•</b> Usage Information: Like most online services, when you use our
-        Service, we automatically collect and store certain information in our
-        server logs. This information helps us make decisions about what we
-        should work on next - for example, by showing which features are most or
-        least popular.
-      </Text>
-      <Text mt={{ base: "1rem", md: "2rem" }}>Examples include:</Text>
-      <Text mt={{ base: "1rem", md: "2rem" }}>
-        Details of how you used our Service, such as your activity on the
-        Service, and the frequency and duration of your visits to the Klosanow
-        Website or Klosanow Web App.
-      </Text>
-      <Text mt={{ base: "1rem", md: "2rem" }}>
-        Device event information such as crashes, system activity, hardware
-        settings,browser type, browser language, the date and time of your
-        request and referral URL.
-      </Text>
-      <Heading
-        fontWeight={700}
-        fontSize={{ base: "3xl", md: "1xl" }}
-        mt={{ base: "1rem", md: "2rem" }}
-      >
-        <Text>Will Klosanow share any information it collects?</Text>
-      </Heading>
-      <Text mt={{ base: "1rem", md: "2rem" }}>
-        Klosanow does not sell or rent your, or your students&apos; personal
-        information to any third party for any purpose - including for
-        advertising or marketing purposes. Furthermore, we do not share personal
-        information with any third parties except in the limited circumstances
-        such as:
-      </Text>
-      <Text mt={{ base: "1rem", md: "1rem" }}>
-        <b>•</b> Aggregated Information and Non-Identifying Information: We may
-        share aggregated, nonpersonally identifiable information publicly,
-        including with uselmrs, partners or the press in order to, for example,
-        demonstrate how Klosanow is used, spot industry trends, or to provide
-        marketing materials for Klosanow. Any aggregated information shared this
-        way will not contain any personal information. If we choose to share
-        this information with any third party, we will take necessary measures
-        to ensure that the third party agrees not to engage in re-identification
-        or the combination of Non-Identifying Information with other data sets
-        that may pose a risk of re-identification.
-      </Text>
-      <Text mt={{ base: "1rem", md: "1rem" }}>
-        <b>•</b> Legal Requirements: We may disclose personal information if we
-        have a good faith belief that doing so is necessary to comply with the
-        law, such as complying with a court order or other legal process. We may
-        need to disclose personal information where, in good faith, we think it
-        is necessary to protect the rights, property, or safety of Klosanow, our
-        employees, our community, or others, or to prevent violations of our
-        Terms of Use or other applicable agreements. This includes,
-        without limitation, exchanging information with other companies and
-        organizations for fraud protection or responding to government requests.
-      </Text>
-      <Heading
-        fontWeight={700}
-        fontSize={{ base: "3xl", md: "1xl" }}
-        mt={{ base: "1rem", md: "2rem" }}
-      >
-        <Text>How does Klosanow protect and secure my information?</Text>
-      </Heading>
-      <Text mt={{ base: "1rem", md: "2rem" }}>
-        Your Klosanow account is protected by a password. You can help us
-        protect your account from unauthorized access by keeping your password
-        secret at all times. The security of your personal information is
-        important to us. We work hard to protect our community, and we maintain
-        administrative, technical and physical safeguards designed to protect
-        against unauthorized use, disclosure of or access to personal
-        information.
-      </Text>
-      <Heading
-        fontWeight={700}
-        fontSize={{ base: "3xl", md: "1xl" }}
-        mt={{ base: "1rem", md: "2rem" }}
-      >
-        <Text>Other Countries</Text>
-      </Heading>
-      <Text mt={{ base: "1rem", md: "2rem" }}>
-        Klosanow is hosted in Nigeria. If you use the Service from any other
-        regions with laws governing data collection, protection and use that may
-        differ from the Nigeria law, please note that your personal information
-        will be transferred outside of those jurisdictions to the Federal
-        Republic of Nigeria. By using the Service, you expressly consent to
-        this, and to the use and storage of personal information in accordance
-        with this Privacy Policy.
-      </Text>
-      <Heading
-        fontWeight={700}
-        fontSize={{ base: "3xl", md: "1xl" }}
-        mt={{ base: "1rem", md: "2rem" }}
-      >
-        <Text>Privacy Policy Changes</Text>
-      </Heading>
-      <Text mt={{ base: "1rem", md: "2rem" }}>
-        Klosanow may, in it sole discretion, modify or revise this Privacy
-        Policy from time to time. We may attempt to notify you when major
-        changes are made to this Privacy Policy, but you should periodically
-        review the most up-to-date version. Your continued use of the Service
-        following the posting of any changes to this Privacy Policy constitutes
-        acceptance of those changes
-      </Text>
-      <Heading
-        fontWeight={700}
-        fontSize={{ base: "3xl", md: "1xl" }}
-        mt={{ base: "1rem", md: "2rem" }}
-      >
-        <Text>DISCLAIMER</Text>
-      </Heading>
-      <Text mt={{ base: "1rem", md: "2rem" }}>
-        By this Policy We do not represent or warrant the condition or
-        functionality of Our platform(s), its suitability for use, nor guarantee
-        that Our service will be free from interruption or any error. No
-        liability or responsibility shall lie for risks associated with the use
-        of Our Platform, including but not limited to any risk to your computer,
-        software or data being damaged by any virus, software, or any other file
-        that might be transmitted or activated via Our Platform or your access
-        to it. Neither do We guarantee the reliability of information contained
-        on Our Platform—particularly those shared by other users.
-      </Text>
-      <Heading
-        fontWeight={700}
-        fontSize={{ base: "3xl", md: "1xl" }}
-        mt={{ base: "1rem", md: "2rem" }}
-      >
-        <Text>Contact Us</Text>
-      </Heading>
-      <Text mt={{ base: "1rem", md: "2rem" }}>
-        If You have any questions about this Privacy Notice, please contact Us
-        at example@email.com
-      </Text>
-    </Box>
-  </>
+  <Box
+    mt={{ base: "1rem", md: "3rem" }}
+    px={{ base: "1rem", md: "6rem" }}
+    py="3rem"
+  >
+    {sections.map((section, idx) => (
+      <React.Fragment key={idx}>
+        {section.title && (
+          <Heading
+            fontWeight={700}
+            fontSize={{ base: "3xl", md: "1xl" }}
+            mt={{ base: "1rem", md: "2rem" }}
+          >
+            <Text>{section.title}</Text>
+          </Heading>
+        )}
+        {section.paragraphs.map((paragraph, i) => (
+          <Text key={i} mt={{ base: "1rem", md: "2rem" }}>
+            {paragraph}
+          </Text>
+        ))}
+      </React.Fragment>
+    ))}
+  </Box>
 );
 
 export default PrivacyPage;
